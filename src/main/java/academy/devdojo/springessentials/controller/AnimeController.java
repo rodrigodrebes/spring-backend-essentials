6821ddaea4e4d44aed985ec70a21c85be9398374
Rodrigo <rodrigodrebes97@gmail.com>
2023-06-09 12:26:04 -0300

    GET method

--- src/main/java/academy/devdojo/springessentials/controller/AnimeController.java
+++ src/main/java/academy/devdojo/springessentials/controller/AnimeController.java
@@ -7,10 +7,10 @@ import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -26,8 +26,13 @@ public class AnimeController {
 
     //localhost:8080/anime/list
     @GetMapping
-    public List<Anime> list(){
+    public ResponseEntity<List<Anime>> list(){
         log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
-        return animeService.listAll();
+        return ResponseEntity.ok(animeService.listAll());
+    }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<Anime> findById(@PathVariable long id){
+        return ResponseEntity.ok(animeService.findById(id));
     }
 }
--- src/main/java/academy/devdojo/springessentials/service/AnimeService.java
+++ src/main/java/academy/devdojo/springessentials/service/AnimeService.java
@@ -1,14 +1,28 @@
 package academy.devdojo.springessentials.service;
 
 import academy.devdojo.springessentials.domain.Anime;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Objects;
 
 // classe esponsável pelas regras/lógica de negócio
 @Service
 public class AnimeService {
+
+    private List<Anime> animes = List.of(new Anime(1L,"Dragon Ball Z"), new Anime(2L,"One Piece"));
+
     public List<Anime> listAll() {
-        return List.of(new Anime(1L, "Dragon Ball Z"), new Anime(2L, "One Piece"));
+        return animes;
     }
+
+    public Anime findById(long id) {
+        return animes.stream()
+                .filter(anime -> Objects.equals(anime.getId(), id))
+                .findFirst()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not found"));
+    }
+
 }
